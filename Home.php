ce2dd2251ceee1b7803059ab703c78680d715e92
AlexSh-l <ashust75@gmail.com>
2020-05-03 18:32:32 +0300

    Lab2

---
+++ Home.php
@@ -0,0 +1,24 @@
+<body class="bodyHome">
+<div class="imgHome">
+</div>
+<header>
+    <nav>
+        <?php include('navcreator.php'); ?>
+    </nav>
+</header>
+<div class="ContentBoxTransparent">
+    <p>Space is the viod that craves exploring...</p><br>
+    <p>Lab2</p><br>
+    <form action="Lab2.php">
+        <input type='number' name='InputAmount'></input>
+        <input type='radio' name="ColorButton" value="Red" checked>Red</input>
+        <input type='radio' name="ColorButton" value="Green">Green</input>
+        <input type='radio' name="ColorButton" value="Blue">Blue</input>
+        <button class='submit'>Enter</button>
+    </form>
+
+</div>
+<footer>
+    @Copyright 2020. All rights reserved.
+</footer>
+</body>
