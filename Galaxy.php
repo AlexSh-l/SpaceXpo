f0b9524fcbe92b9fe3312a40c4f81b6294c11359
AlexSh-l <ashust75@gmail.com>
2020-05-09 19:05:01 +0300

    Lab5

---
+++ Galaxy.php
@@ -0,0 +1,41 @@
+<body class="bodyGalaxy">
+<div class="imgGalaxy">
+</div>
+<header>
+    <nav>
+        <?php include('navcreator.php'); ?>
+    </nav>
+</header>
+<div class="ContentBoxTransparent">
+    <ol>Milky way - structure:
+        <li>Galactic quadrants
+            <ul>
+                <li>1st galactic quadrant(0..90 degrees)</li>
+                <li>2nd galactic quadrant(90..180 degrees)</li>
+                <li>3rd galactic quadrant(180..270 degrees)</li>
+                <li>4st galactic quadrant(270..360 degrees)</li>
+            </ul>
+        </li>
+        <li>Galactic Center
+            <ul>
+                <li>Saggitarius A*(which harbours a supermassive black hole)</li>
+            </ul>
+        </li>
+        <li>Spiral arms(gas, dust and star formations)
+            <ul></ul>
+        </li>
+        <li>Halo(old stars, global clusters)
+            <ul></ul>
+        </li>
+        <ul>
+            <li>Gaseous halo</li>
+        </ul>
+        <li>Galactic rotation(between 210+-10 km/s)
+            <ul></ul>
+        </li>
+    </ol>
+</div>
+<footer>
+    @Copyright 2020. All rights reserved.
+</footer>
+</body>
