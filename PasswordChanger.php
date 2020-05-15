fe33f0dd8101c33b0e96ee5127695bdc1337ee39
AlexSh-l <ashust75@gmail.com>
2020-05-15 08:27:31 +0300

    Lab8

---
+++ PasswordChanger.php
@@ -0,0 +1,26 @@
+<head>
+    <meta charset="UTF-8">
+    <title>SpaceXpo</title>
+    <link rel="stylesheet" type="text/css" href="styling.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body class="bodySurvey">
+<div class="imgSurvey">
+</div>
+<div class="ContentBoxTransparent">
+    <div class="bodyRegistration">
+        <div class="registrationbox">
+            <div class="greeter">Enter your information</div>
+            <form method="post" action="Lab5.php">
+                <br><input type="text" name="Name" placeholder="Name"></input><br>
+                <input type="password" name="oldPassword" placeholder="Old password"></input><br>
+                <input type="password" name="newPassword" placeholder="New password"></input><br>
+                <button type="submit">Enter Information</button>
+            </form>
+        </div>
+    </div>
+</div>
+<footer>
+    @Copyright 2020. All rights reserved.
+</footer>
+</body>
---
+++ users.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Май 08 2020 г., 23:02
+-- Версия сервера: 10.4.11-MariaDB
+-- Версия PHP: 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `registr-bd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `passwrd` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `passwrd`) VALUES
+(8, 'Andrew', '8cb2237d0679ca88db6464eac60da96345513964'),
+(9, 'Admin', '20eabe5d64b0e216796e834f52d61fd0b70332fc'),
+(10, 'Viktor', 'f0b9995d3e02a1cc6374d455ea78edc8dcdcf46a');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
